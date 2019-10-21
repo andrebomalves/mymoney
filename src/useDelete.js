54684228643deec22a8146d5c3860d8b36d3c3f7
Andre Bom Alves Nunes <andrebom@MacBook-Air-de-Andre.local>
2019-10-21 13:24:20 -0300

    add useDelete

---
+++ src/useDelete.js
@@ -0,0 +1,30 @@
+import {useReducer,useEffect} from 'react'
+import axios from 'axios'
+
+const reducer = (state, action) => {
+    switch (action.type) {
+      case 'REQUESTS':
+        return { ...state, loading: true};
+      case 'SUCCESS':
+        return {...state, loading:false, data:action.data};
+      default:
+        return state
+    }
+  }
+  
+  
+  
+  const useDelete = url => {
+    const [data, dispatch] = useReducer(reducer,{loading:true, data:{}});
+  
+      dispatch({type:'REQUEST'})
+      axios.delete(url,)
+        .then((res) => {
+          console.log(res)
+          dispatch({type:'SUCCESS', data:res.data})
+        }) 
+  
+    return data
+  } 
+
+  export default useDelete
